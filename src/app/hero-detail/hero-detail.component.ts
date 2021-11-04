4586a1c8627b035bb1706918ba6633be185674ad
Milosh Charapikj <milos.carapic@sourcico.com>
2021-11-04 09:05:08 +0100

    initial commit

---
+++ src/app/hero-detail/hero-detail.component.ts
@@ -0,0 +1,24 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {Hero} from "../entity/hero";
+import {HeroService} from "../hero.service";
+
+@Component({
+  selector: 'app-hero-detail',
+  templateUrl: './hero-detail.component.html',
+  styleUrls: ['./hero-detail.component.css']
+})
+export class HeroDetailComponent implements OnInit {
+  @Input() hero?: Hero;
+  heroes: Hero[] = [];
+
+  constructor(private heroService: HeroService) {}
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
+  }
+}
